9db7beb353359412173251dc45c24400a3515976
Marten Deinum <mdeinum@gmail.com>
2017-04-30 17:05:34 +0200

    Added LDAP (OpenDS) Docker Script

---
+++ ch07/recipe_7_1_iii/src/main/java/com/apress/springrecipes/board/web/TodoWebConfig.java
@@ -0,0 +1,42 @@
+package com.apress.springrecipes.board.web;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.apress.springrecipes.board")
+public class TodoWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/todos");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("board")
+                .addScript("classpath:/schema.sql")
+                .build();
+    }
+}
---
+++ ch07/recipe_7_1_iii/src/main/java/com/apress/springrecipes/board/web/TodoWebInitializer.java
@@ -0,0 +1,32 @@
+package com.apress.springrecipes.board.web;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class TodoWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{ TodoWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[] {
+                new HiddenHttpMethodFilter()
+        };
+    }
+}
