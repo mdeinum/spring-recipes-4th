0fe6aa132e3ae629dfa7061587b9d91072414a69
Marten Deinum <mdeinum@gmail.com>
2017-05-09 11:46:43 +0200

    Parameterize script

--- bin/postgres.sh
+++ bin/postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name sr4-postgres -e POSTGRES_PASSWORD=password -p 5432:5432 postgres
+docker run --name sr4-postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
