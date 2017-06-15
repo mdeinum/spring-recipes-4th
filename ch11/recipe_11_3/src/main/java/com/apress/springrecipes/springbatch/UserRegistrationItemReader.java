a90cf2ba7c5ad9c65e28543c9c897e199ee1d0d4
Marten Deinum <mdeinum@gmail.com>
2017-06-15 21:36:29 +0200

    CH11: WIP

---
+++ ch11/recipe_11_3/src/main/java/com/apress/springrecipes/springbatch/UserRegistrationItemReader.java
@@ -0,0 +1,25 @@
+package com.apress.springrecipes.springbatch;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+import java.util.Date;
+
+
+public class UserRegistrationItemReader implements ItemReader<UserRegistration> {
+
+    @Autowired
+    private UserRegistrationService userRegistrationService;
+
+    public UserRegistration read() throws Exception {
+        Date today = new Date();
+        Collection<UserRegistration> registrations = userRegistrationService.getOutstandingUserRegistrationBatchForDate(1, today);
+
+        if ((registrations != null) && (registrations.size() >= 1)) {
+            return registrations.iterator().next();
+        }
+
+        return null;
+    }
+}
