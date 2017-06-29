fe618fc80f074d1933bb978c2c305ea915dc0ae7
Marten Deinum <mdeinum@gmail.com>
2017-06-29 20:51:07 +0200

    CH05: WIP
    ARGH Reactive...

--- ch05/recipe_5_5_ii/src/main/java/com/apress/springrecipes/websocket/EchoHandler.java
+++ ch05/recipe_5_5_ii/src/main/java/com/apress/springrecipes/websocket/EchoHandler.java
@@ -1,7 +1,7 @@
 package com.apress.springrecipes.websocket;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,18 +10,20 @@ import reactor.core.publisher.Mono;
 public class EchoHandler {
 
     @RequestMapping("/echo")
-    public Mono<String> echo(@RequestParam("msg") String msg) {
-        return Mono.just(msg);
+    public Mono<OutputText> echo(@RequestBody Mono<InputText> input) {
+        return input.map(m -> new OutputText(m.getMsg()));
     }
 
     @RequestMapping("/reverse")
-    public Mono<String> reverse(@RequestParam("msg") String msg) {
-        return Mono.just(msg).map(s -> new StringBuilder(s).reverse().toString());
+    public Mono<OutputText> reverse(@RequestBody Mono<InputText> input) {
+        return input.map(InputText::getMsg).map(t -> new StringBuilder(t).reverse().toString()).map(OutputText::new);
     }
 
     @RequestMapping("/echo-chars")
-    public Flux<String> echoChars(@RequestParam("msg") String msg) {
+    public Flux<OutputText> echoChars(@RequestBody Mono<InputText> input) {
 
-        return Flux.fromStream(msg.chars().mapToObj(i -> (char) i).map(String::valueOf));
+        Flux.from(input)
+
+        return input.flatMap(s -> Flux.fromArray(() -> s.getMsg().split(""))).map(OutputText::new);
     }
 }
