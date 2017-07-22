1d1343db6caf350a832ced2cb8baa5f9ff25660c
Marten Deinum <mdeinum@gmail.com>
2017-07-22 19:12:20 +0200

    CH12: Reactive Additional (WIP)

--- ch12/recipe_12_1_vi/src/main/java/com/apress/springrecipes/nosql/Main.java
+++ ch12/recipe_12_1_vi/src/main/java/com/apress/springrecipes/nosql/Main.java
@@ -1,44 +1,40 @@
 package com.apress.springrecipes.nosql;
 
 import com.apress.springrecipes.nosql.config.MongoConfiguration;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by marten on 22-09-14.
  */
 public class Main {
 
-    public static final String DB_NAME = "vehicledb";
-
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfiguration.class);
         VehicleRepository repository = ctx.getBean(VehicleRepository.class);
 
-        repository.count().
-                subscribe(cnt -> System.out.println("Number of Vehicles: " + cnt.longValue()));
+        CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        repository.saveAll(
-                Flux.fromIterable(
-                        Arrays.asList(
+        repository.count().doOnSuccess(cnt -> System.out.println("Number of Vehicles: " + cnt))
+                .thenMany(        repository.saveAll(
+                        Flux.just(
+
                                 new Vehicle("TEM0001", "RED", 4, 4),
-                                new Vehicle("TEM0002", "RED", 4, 4)))).subscribe();
+                                new Vehicle("TEM0002", "RED", 4, 4))))
+                .last()
+                .then(repository.count()).doOnSuccess(cnt -> System.out.println("Number of Vehicles: " + cnt))
+                .then(repository.findByVehicleNo("TEM0001")).doOnSuccess(System.out::println)
+                .then(repository.deleteAll())
+                    .doOnSuccess(x -> countDownLatch.countDown())
+                    .doOnError(t -> countDownLatch.countDown())
+                .then(repository.count()).subscribe(cnt -> System.out.println("Number of Vehicles: " + cnt.longValue()));
 
-        repository.count().subscribe(cnt -> System.out.println("Number of Vehicles: " + cnt.longValue()));
 
-        repository.findByVehicleNo("TEM0001").subscribe(v -> ToStringBuilder.reflectionToString(v, ToStringStyle.SHORT_PREFIX_STYLE));
-
-
-        repository.deleteAll().block();
-
-        repository.count().subscribe(cnt -> System.out.println("Number of Vehicles: " + cnt.longValue()));
-
+        countDownLatch.await();
         ((AbstractApplicationContext) ctx).close();
 
     }
